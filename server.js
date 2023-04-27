c6270f7b7ada12244d1b29ffdbfa7d1c9e2d0454
cmcgill436 <candra.mcgill@gmail.com>
2023-04-27 09:41:06 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -12,11 +12,44 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`What's up, ${req.params.name}`);
 });
 
+//tip calculator
 app.get("/tip/:total/:tipPercentage", (req, res) => {
   const total = req.params.total;
   const tipPercentage = req.params.tipPercentage / 100;
   const tipAmount = total * tipPercentage;
   res.send(tipAmount.toFixed(2).toString());
+});
+
+//magic 8 ball
+
+const responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic/:question", (req, res) => {
+  console.log(req.params);
+  res.send(
+    `<h1>${responses[Math.floor(Math.random() * responses.length)]}</h1>`
+  );
 });
 
 app.listen(PORT, () => {
